6169c43476924714348719da98cce42ca8617b9f
ChiragDAD01 <chiragdadhich0@gmail.com>
2025-04-11 00:10:08 +0530

    fix: added types for mesh and sphere elements

--- types/r3f-elements.d.ts
+++ types/r3f-elements.d.ts
@@ -3,8 +3,11 @@ import '@react-three/fiber'
 declare module 'react' {
   namespace JSX {
     interface IntrinsicElements {
-      ambientLight: React.DetailedHTMLProps<any, any>
-      pointLight: React.DetailedHTMLProps<any, any>
+      mesh: any
+      ambientLight: any
+      pointLight: any
+      sphereGeometry: any
+      meshStandardMaterial: any
     }
   }
 }
