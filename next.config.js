e53fe221ef71337fcaf45c375b6eec64deef0532
ChiragDAD01 <chiragdadhich0@gmail.com>
2025-04-17 11:06:21 +0530

    chore: update build and manifest files for improved structure

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  turbopack: {
-    // Turbopack configuration options
-  }
+  // turbopack: {
+  //   // Turbopack configuration options
+  // }
 }
 
 module.exports = nextConfig
