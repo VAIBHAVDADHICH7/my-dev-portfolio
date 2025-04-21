4ed40717ebdf7888f76b656420ec9e77df4dadaa
ChiragDAD01 <chiragdadhich0@gmail.com>
2025-04-22 03:07:29 +0530

    Add webpack chunk for optimized JavaScript bundling

--- .next/static/chunks/app_page_tsx_e6386b64._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/components_Skills3D_tsx_d5ba4eda._.js",
-  "static/chunks/_7a246e2c._.js"
-],
-    source: "dynamic"
-});
